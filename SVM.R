52a20b00070af78114c82e6a29e9da00b1d7ee54
Joseph Bradley <hermite2010@gmail.com>
2023-10-26 10:59:08 -0600

    reduced leves and tunes on SVM.R

--- SVM.R
+++ SVM.R
@@ -28,21 +28,21 @@ baked_data <- bake(prep, new_data=AEAC_Train)
 # SVM ---------------------------------------------------
 
 ## SVM models
-#svmPoly <- svm_poly(degree=tune(), cost=tune()) %>% # set or tune
-#  set_mode("classification") %>%
-#  set_engine("kernlab")
+svmPoly <- svm_poly(degree=tune(), cost=tune()) %>% # set or tune
+  set_mode("classification") %>%
+  set_engine("kernlab")
 
-#svmPoly_wf <- workflow() %>%
-#  add_recipe(AEAC_recipe) %>%
-#  add_model(svmPoly)
+svmPoly_wf <- workflow() %>%
+  add_recipe(AEAC_recipe) %>%
+  add_model(svmPoly)
 
-#svmRadial <- svm_rbf(rbf_sigma=tune(), cost=tune()) %>% # set or tune
-#  set_mode("classification") %>%
-#  set_engine("kernlab")
+svmRadial <- svm_rbf(rbf_sigma=tune(), cost=tune()) %>% # set or tune
+  set_mode("classification") %>%
+  set_engine("kernlab")
 
-#svmRadial_wf <- workflow() %>%
-#  add_recipe(AEAC_recipe) %>%
-#  add_model(svmRadial)
+svmRadial_wf <- workflow() %>%
+  add_recipe(AEAC_recipe) %>%
+  add_model(svmRadial)
 
 svmLinear <- svm_linear(cost=tune()) %>% # set or tune
   set_mode("classification") %>%
@@ -54,18 +54,18 @@ svmLinear_wf <- workflow() %>%
 
 ## set up grid of tuning values
 
-#svmPoly_grid <- grid_regular(degree(),
-#                             cost(),
-#                             levels = 10)
-#svmRadial_grid <- grid_regular(rbf_sigma(),
-#                               cost(),
-#                               levels = 10)
+svmPoly_grid <- grid_regular(degree(),
+                             cost(),
+                             levels = 3)
+svmRadial_grid <- grid_regular(rbf_sigma(),
+                               cost(),
+                               levels = 3)
 svmLinear_grid <- grid_regular(cost(),
-                               levels = 10)
+                               levels = 3)
 
 ## set up k-fold CV
 
-svm_folds <- vfold_cv(AEAC_Train, v = 10, repeats=1)
+svm_folds <- vfold_cv(AEAC_Train, v = 5, repeats=1)
 
 ## Set up Parallel processing
 
@@ -77,15 +77,15 @@ registerDoParallel(cl)
 
 ## Run the CV
 
-#svmPoly_results <- svmPoly_wf %>%
-#  tune_grid(resamples=svm_folds,
-#            grid=svmPoly_grid,
-#            metrics=metric_set(roc_auc)) #Or leave metrics NULL
+svmPoly_results <- svmPoly_wf %>%
+  tune_grid(resamples=svm_folds,
+            grid=svmPoly_grid,
+            metrics=metric_set(roc_auc)) #Or leave metrics NULL
 
-#svmRadial_results <- svmRadial_wf %>%
-#  tune_grid(resamples=svm_folds,
-#            grid=svmRadial_grid,
-#            metrics=metric_set(roc_auc))
+svmRadial_results <- svmRadial_wf %>%
+  tune_grid(resamples=svm_folds,
+            grid=svmRadial_grid,
+            metrics=metric_set(roc_auc))
             
 svmLinear_results <- svmLinear_wf %>%
   tune_grid(resamples=svm_folds,
@@ -95,44 +95,44 @@ svmLinear_results <- svmLinear_wf %>%
 stopCluster(cl)
 ## find best tuning parameters
 
-#polyBestTune <- svmPoly_results %>%
-#  select_best("roc_auc")
+polyBestTune <- svmPoly_results %>%
+  select_best("roc_auc")
 
-#radialBestTune <- svmRadial_results %>%
-#  select_best("roc_auc")
+radialBestTune <- svmRadial_results %>%
+  select_best("roc_auc")
 
 linearBestTune <- svmLinear_results %>%
   select_best("roc_auc")
 
 ## Finalize workflow and prediction 
 
-#polyFinal_wf <- svmPoly_wf %>%
-#  finalize_workflow(polyBestTune) %>%
-#  fit(data=AEAC_Train)
+polyFinal_wf <- svmPoly_wf %>%
+  finalize_workflow(polyBestTune) %>%
+  fit(data=AEAC_Train)
 
-#radialFinal_wf <- svmRadial_wf %>%
-#  finalize_workflow(radialBestTune) %>%
-#  fit(data=AEAC_Train)
+radialFinal_wf <- svmRadial_wf %>%
+  finalize_workflow(radialBestTune) %>%
+  fit(data=AEAC_Train)
 
 linearFinal_wf <- svmLinear_wf %>%
   finalize_workflow(linearBestTune) %>%
   fit(data=AEAC_Train)
 
-#svmPoly_preds <- polyFinal_wf %>%
-#  predict(new_data = AEAC_Test, type="prob") %>% 
-#  mutate(id = row_number()) %>% 
-#  rename(Action = .pred_1) %>% 
-#  select(3,2)
+svmPoly_preds <- polyFinal_wf %>%
+  predict(new_data = AEAC_Test, type="prob") %>% 
+  mutate(id = row_number()) %>% 
+  rename(Action = .pred_1) %>% 
+  select(3,2)
 
-#vroom_write(x=svmPoly_preds, file="SVM_Poly.csv", delim=",") #"Amazon_AEAC_Kaggle/KNN.csv"
+vroom_write(x=svmPoly_preds, file="SVM_Poly.csv", delim=",") #"Amazon_AEAC_Kaggle/SVM_Poly.csv"
 
-#svmRadial_preds <- radialFinal_wf %>%
-#  predict(new_data = AEAC_Test, type="prob") %>% 
-#  mutate(id = row_number()) %>% 
-#  rename(Action = .pred_1) %>% 
-#  select(3,2)
+svmRadial_preds <- radialFinal_wf %>%
+  predict(new_data = AEAC_Test, type="prob") %>% 
+  mutate(id = row_number()) %>% 
+  rename(Action = .pred_1) %>% 
+  select(3,2)
 
-#vroom_write(x=svmRadial_preds, file="SVM_Radial.csv", delim=",")
+vroom_write(x=svmRadial_preds, file="SVM_Radial.csv", delim=",")
 
 svmLinear_preds <- linearFinal_wf %>%
   predict(new_data = AEAC_Test, type="prob") %>% 
